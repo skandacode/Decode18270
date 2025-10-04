ec1cfd8e0f45cac982d2200f9261a666c868991a
skandacode <39654761+skandacode@users.noreply.github.com>
2025-10-04 08:37:08 -0500

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LibTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LibTesting.java
@@ -3,8 +3,15 @@ package org.firstinspires.ftc.teamcode;
 import com.bylazar.ftcontrol.panels.Panels;
 import com.bylazar.ftcontrol.panels.configurables.annotations.Configurable;
 import com.bylazar.ftcontrol.panels.integration.TelemetryManager;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import solverslib.hardware.Cached2mDistance;
+import solverslib.hardware.CachedColorSensorV3;
 
 @Configurable
 @TeleOp
@@ -14,18 +21,31 @@ public class LibTesting extends LinearOpMode {
 
     public static double testVar = 0.0;
 
+    CachedColorSensorV3 color;
+    Cached2mDistance distance;
 
     @Override
     public void runOpMode() {
         panelsTelemetry = Panels.getTelemetry();
         panelsTelemetry.debug("Init was ran!");
         panelsTelemetry.update(telemetry);
+
+        color = (CachedColorSensorV3) hardwareMap.get(RevColorSensorV3.class, "color");
+        distance = (Cached2mDistance) hardwareMap.get(Rev2mDistanceSensor.class, "distance");
+
         waitForStart();
 
         while (opModeIsActive()) {
             panelsTelemetry.debug("This is a line!");
             panelsTelemetry.debug("Here is a variable: " + Math.PI);
             panelsTelemetry.debug("Test variable: " + testVar);
+            panelsTelemetry.debug();
+            panelsTelemetry.debug("Color Value: "+color.getDistance(DistanceUnit.INCH));
+            panelsTelemetry.debug("Color last refresh: "+color.getLastRefresh());
+            panelsTelemetry.debug();
+            panelsTelemetry.debug("Distance Value: "+distance.getDistance(DistanceUnit.INCH));
+            panelsTelemetry.debug("Distance last refresh: "+distance.getLastRefresh());
+            panelsTelemetry.debug();
             panelsTelemetry.update(telemetry);
         }
     }
